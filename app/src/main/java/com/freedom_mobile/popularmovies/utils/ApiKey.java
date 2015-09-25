d449e480e8bf6c68612786970eda31989f047506
Q-Leito <q_leito@live.nl>
2015-09-25 21:47:58 +0200

    Updated code.

--- app/src/main/java/com/freedom_mobile/popularmovies/utils/ApiKey.java
+++ app/src/main/java/com/freedom_mobile/popularmovies/utils/ApiKey.java
@@ -1,5 +1,5 @@
 package com.freedom_mobile.popularmovies.utils;
 
-public class ApiKey {
-    public static final String API_KEY = "aaa72b63cb24a921caf0d85ad5decf78bce";
+public final class ApiKey {
+    public static final String API_KEY = "72b63cb24a921caf0d85ad5decf78bce";
 }
